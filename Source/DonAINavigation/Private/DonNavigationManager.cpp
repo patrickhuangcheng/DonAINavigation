8d436f8f7219bad2513481d4e83bf44650c4758b
Sheppy <sheppy@atlantishq.de>
2024-09-13 21:15:37 +0200

    fix: unreachable return & ambigious call for ue_5.4

--- Source/DonAINavigation/Private/DonNavigationManager.cpp
+++ Source/DonAINavigation/Private/DonNavigationManager.cpp
@@ -773,12 +773,9 @@ bool ADonNavigationManager::ScheduleDynamicCollisionUpdate(UPrimitiveComponent* 
 	if(bNeedsToScheduleTask)
 		AddDynamicCollisionTask(task);
 
-	return bOverallStatus;	
-
 	UE_LOG(DoNNavigationLog, Verbose, TEXT("Num collision tasks: %d"), ActiveDynamicCollisionTasks.Num());
 
-	return true;
-
+	return bOverallStatus;
 }
 
 bool ADonNavigationManager::IsDynamicCollisionTaskActive(const FDonNavigationDynamicCollisionTask& Task)
@@ -2850,7 +2847,7 @@ FVector ADonNavigationManager::FindRandomPointAroundOriginInNavWorld(AActor* Nav
 	for (int32 i = 0; i < MaxAttempts; i++)
 	{
 		float maxZAngularDispacement = FMath::Abs(MaxZAngularDispacement);
-		FRotator newDirection = FRotator(FMath::FRandRange(-maxZAngularDispacement, maxZAngularDispacement), FMath::FRandRange(0, 360), FMath::FRandRange(0, 360));
+		FRotator newDirection = FRotator(FMath::FRandRange(-maxZAngularDispacement, maxZAngularDispacement), FMath::FRandRange(0.0, 360.0), FMath::FRandRange(0.0, 360.0));
 		newDestination = Origin + newDirection.RotateVector(baseDisplacement);
 
 		if (MaxDesiredAltitude != -1.f)
